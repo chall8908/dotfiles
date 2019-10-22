5ae6e3efd44aed7b3f5c0077812604d2b447f153
Chris Hall <chall8908@gmail.com>
2019-10-22 16:44:48 -0500

    Re-enable enh-ruby-mode
    It's still a bit slow opening new files, but it fixes a bunch of
    highlighting and formatting issues.
    
    [emacs]

--- emacs/config/modes/ruby.el
+++ emacs/config/modes/ruby.el
@@ -6,21 +6,21 @@
 
 (use-package rvm)
 
-;; (use-package enh-ruby-mode
-;;   ;; use on most ruby based files
-;;   :mode "\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'"
-;;   :interpreter "ruby"
-;;   ;; might need this if it fucks with base16
-;;   :config
-;;   (remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
-;;   )
+(use-package enh-ruby-mode
+  ;; use on most ruby based files
+  :mode "\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|plan\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'"
+  :interpreter "ruby"
+  ;; might need this if it fucks with base16
+  :config
+  (remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
+  )
 
-(add-to-list 'auto-mode-alist
-             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|plan\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
+;; (add-to-list 'auto-mode-alist
+;;              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|plan\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
 
 (use-package ruby-electric
   :diminish
-  :hook (ruby-mode . ruby-electric-mode)
+  :hook (enh-ruby-mode . ruby-electric-mode)
   )
 
 (use-package rspec-mode
