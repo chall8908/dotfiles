e5df435bb347177ca1792b64078c1db518341a57
Chris Hall <chall8908@gmail.com>
2020-11-17 17:07:30 -0600

    Fix projectile mode keymap
    [emacs]

--- emacs/config/init-ivy.el
+++ emacs/config/init-ivy.el
@@ -7,7 +7,7 @@
   :diminish
   ;; :bind-keymap ("C-c p" . projectile-command-map)
   :config
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
   ;; (projectile-mode +1)
   )
 
